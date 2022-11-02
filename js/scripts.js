087d70004523b66e6af4639076760d956c511ea2
Elguedo <114593149+Elguedo@users.noreply.github.com>
2022-11-01 19:10:42 -0500

    Elguedo
    agregue una nueva funcionalidad al login

--- js/scripts.js
+++ js/scripts.js
@@ -42,8 +42,31 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
+
+
+
 //TO DO
 // Declare las variables del formulario login
+// Ontenemos los valores de los campos de contraseñas 
+const boton=document.getElementById('loginBtn')
 
-// Implemente el evento para validar
+//cree la funcion validar
+function validar() {
+const email=document.getElementById("inputEmail").value;
+const pass= document.getElementById("inputPassword").value;
+  if(email =="admin" && pass=="12345"){
+    window.location.href="/pages/dashboard.html"
+    }
+     else {
+      alert("Usuario Incorrecto")   
+    }
+
+  }
+ // Implemente el evento para llamar a la funcion validar
 // btn.addEventListener("click", function (evt) ....
+  boton.addEventListener("click", validar);
+  
+
+
+
+
